19609a16dd471dd006d2d17088d4faf90eb6ba20
kozmailona <kozmailona12@gmail.com>
2020-03-01 16:59:01 +0200

    changed data types

--- src/main/java/org/fasttrackit/Grooming.java
+++ src/main/java/org/fasttrackit/Grooming.java
@@ -2,10 +2,10 @@ package org.fasttrackit;
 
 public class Grooming {
 
-    String bathing;
-    String hairRemoval;
-    String nailTrimming;
-    String earCleaning;
+    boolean bathing;
+    boolean hairRemoval;
+    boolean nailTrimming;
+    boolean earCleaning;
     int bathPrice;
     int hairRemovalPrice;
     int nailTrimmingPrice;
