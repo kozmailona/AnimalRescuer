947da670df182c6ec57a76afe5a09a294dc1655b
kozmailona <kozmailona12@gmail.com>
2020-03-16 02:05:10 +0200

    inheritance - subclass added

--- src/main/java/org/fasttrackit/Dog.java
+++ src/main/java/org/fasttrackit/Dog.java
@@ -1,4 +1,10 @@
 package org.fasttrackit;
 
-public class Dog {
+public class Dog extends Animal {
+
+    int senseOfSmell;       // int senseOfSmell = 10; on a 1-10 scale for example
+
+    public void barking() {
+        System.out.println("The dog is barking.");
+    }
 }
