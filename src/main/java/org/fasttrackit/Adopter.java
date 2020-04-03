b95dd4917dc2a166314ee28542d685a343ff0bbe
kozmailona <kozmailona12@gmail.com>
2020-04-03 19:32:06 +0300

    if and if else added

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -12,15 +12,31 @@ public class Adopter {
 
     }
     public void feed (Animal animal, Food food) {
+
         System.out.println(name + " just gave some " + food.name + " to " + animal.name);
         animal.hungerLevel = animal.hungerLevel -5;
+        animal.spiritLevel = 3;
+
+        if (animal.favouriteFood == food.name) {
+            animal.spiritLevel++;
+            System.out.println("Now Jack is happy. This much: " + animal.spiritLevel + ".");
+        }
 
     }
 
     public void playing(Animal animal, Activity activity) {
+        animal.spiritLevel = 3;
         System.out.println(name + " was " + activity.name + " with " + animal.name + " in the " + activity.location +
                 ".");
         animal.spiritLevel = animal.spiritLevel +5;
+
+        if (activity.name == animal.favouriteActivity) {
+            animal.spiritLevel++;
+            System.out.println("Yaay, I love it. I'm happy. This muuch: " + animal.spiritLevel);
+        } else {
+            animal.spiritLevel--;
+            System.out.println("Oh, I don't like it... I'm not in my mood: " + animal.spiritLevel);
+        }
     }
 }
 
