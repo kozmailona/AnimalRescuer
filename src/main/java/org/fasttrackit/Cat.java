97dfdc6dbcf34562ef4c745824adadcb1637cace
kozmailona <kozmailona12@gmail.com>
2020-05-19 23:27:13 +0300

    getter-setter and field added to cat class

--- src/main/java/org/fasttrackit/Cat.java
+++ src/main/java/org/fasttrackit/Cat.java
@@ -1,4 +1,19 @@
 package org.fasttrackit;
 
-public class Cat {
+public class Cat extends Animal {
+
+    boolean purring;
+
+    public Cat(String name, String colour, String species, String favouriteFood, String favouriteActivity, int age,
+               int healthLevel, int hungerLevel, int spiritLevel) {
+        super(name, colour, species, favouriteFood, favouriteActivity, age, healthLevel, hungerLevel, spiritLevel);
+    }
+
+    public boolean isPurring() {
+        return purring;
+    }
+
+    public void setPurring(boolean purring) {
+        this.purring = purring;
+    }
 }
